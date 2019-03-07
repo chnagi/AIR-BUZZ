3baf158bf2718843027c5f61133527a0df5b0af0
chnagi <nagalakshmi.c15@iiits.in>
2019-03-07 10:36:57 +0530

    API VIEWS for AirCrafts and Flights

--- cms/serializers.py
+++ cms/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from .models import *
 
-class AirCrafts(serializers.ModelSerializer):
+class AirCraftsSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=AirCrafts
 		fields='__all__'
-class Flights(serializers.ModelSerializer):
+class FlightsSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=Flights
 		fields='__all__'
--- cms/views.py
+++ cms/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
-
+from . models import *
+from . serializers import *
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
 # Create your views here.
 def index(request):
     return render(request,"cms/index.html")
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def AirCrafts_detail(request):
+    Aircraft_data=AirCrafts.obects.all()
+    if request.method == 'GET':
+        serializer = AirCraftsSerializer(Aircraft_data)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = Serializer(Aircraft_data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def Flights_detail(request):
+    flight_data=Flights.obects.all()
+    if request.method == 'GET':
+        serializer = AirCraftsSerializer(flight_data)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = Serializer(flight_data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
