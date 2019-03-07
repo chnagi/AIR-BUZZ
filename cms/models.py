38903e7839bc325b199b3f3c3f7d17370697cfab
chnagi <nagalakshmi.c15@iiits.in>
2019-03-07 11:20:28 +0530

    change made in model
    Signed-off-by: chnagi <nagalakshmi.c15@iiits.in>

--- cms/models.py
+++ cms/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class AirCrafts(models.Model):
 	MSN = models.IntegerField(primary_key=True,validators=[MinValueValidator(0)])
-	Name = models.CharField(max_length=100)
+	Name = models.CharField(max_length=100,unique=True)
 class Flights(models.Model):
 	MSN=models.ForeignKey(AirCrafts,on_delete=models.CASCADE)
 	HarnessLength = models.IntegerField(default=0,validators=[ MinValueValidator(0)])
@@ -14,7 +14,7 @@ class Flights(models.Model):
 	FuelQuantity_left=models.IntegerField(default=0,validators=[MinValueValidator(0)])
 	FuelQuantity_right=models.IntegerField(default=0,validators=[MinValueValidator(0)])
 	Max_Altitude=models.IntegerField(default=0,validators=[MinValueValidator(0)])
-	Flight_No=models.CharField(max_length=100)
+	Flight_No=models.CharField(max_length=100,unique=True)
 class NewsFeed(models.Model):
 	Title=models.CharField(max_length=100)
 	Author=models.CharField(max_length=100)
