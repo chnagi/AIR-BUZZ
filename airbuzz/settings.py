cc6c83447801c91f999709f8c54939fd7da93a80
vaishnavm217 <vaishnavmurali@gmail.com>
2019-03-07 13:41:16 +0530

    Cors fix
    Signed-off-by: vaishnavm217 <vaishnavmurali@gmail.com>

--- airbuzz/settings.py
+++ airbuzz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'cms',
     'rest_framework',
     'django.contrib.admin',
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'airbuzz.urls'
-
+CORS_ORIGIN_ALLOW_ALL=True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
