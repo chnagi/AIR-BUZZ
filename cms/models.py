0e040c87803758b8972da0768a84276789ff6d0d
chnagi <nagalakshmi.c15@iiits.in>
2019-03-07 10:13:42 +0530

    Corrections
    Signed-off-by: chnagi <nagalakshmi.c15@iiits.in>

--- cms/models.py
+++ cms/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 class AirCrafts(models.Model):
-	MSN = models.IntegerField(max_length=100,primary_key=True,reuired=True,validators=[MinValueValidator(0)])
-	Name = models.CharField(max_length=100,required=True)
+	MSN = models.IntegerField(primary_key=True,validators=[MinValueValidator(0)])
+	Name = models.CharField(max_length=100)
 class Flights(models.Model):
-	MSN=models.ForeignKey(AirCrafts,on_delete=models.CASCADE,required=True)
-	HarnessLength = models.IntegerField(default=0,required=True,validators=[ MinValueValidator(0)])
-	GrossWeight = models.IntegerField(default=0,required=True,validators=[MinValueValidator(0)])
-	AtmosphericPressure = models.IntegerField(default=0,required=True,validators=[MinValueValidator(0)])
-	FuelCapacity_left=models.IntegerField(default=0,required=True,validators=[MinValueValidator(0)])
-	FuelCapacity_right=models.IntegerField(default=0,required=True,validators=[MinValueValidator(0)])
-	FuelQuantity_left=models.IntegerField(default=0,required=True,validators=[MinValueValidator(0)])
-	FuelQuantity_right=models.IntegrField(default=0,required=True),validators=[MinValueValidator(0)]
-	Max_Altitude=models.IntegerField(default=0,required=True,validators=[MinValueValidator(0)])
-	Flight_No=models.CharField(max_length=100,required=True)
+	MSN=models.ForeignKey(AirCrafts,on_delete=models.CASCADE)
+	HarnessLength = models.IntegerField(default=0,validators=[ MinValueValidator(0)])
+	GrossWeight = models.IntegerField(default=0,validators=[MinValueValidator(0)])
+	AtmosphericPressure = models.IntegerField(default=0,validators=[MinValueValidator(0)])
+	FuelCapacity_left=models.IntegerField(default=0,validators=[MinValueValidator(0)])
+	FuelCapacity_right=models.IntegerField(default=0,validators=[MinValueValidator(0)])
+	FuelQuantity_left=models.IntegerField(default=0,validators=[MinValueValidator(0)])
+	FuelQuantity_right=models.IntegerField(default=0,validators=[MinValueValidator(0)])
+	Max_Altitude=models.IntegerField(default=0,validators=[MinValueValidator(0)])
+	Flight_No=models.CharField(max_length=100)
